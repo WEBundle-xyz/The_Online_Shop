7b7ae81aef6a217eed7e10db6361284401d2c25e
STEF G <stefano@webundlexyzsmbp.mynetworksettings.com>
2022-02-09 14:09:33 -0500

    User login-logout button functionality

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const express = require('express');
-
 const csrf = require('csurf');
 const expressSession = require('express-session');
 
@@ -9,8 +8,9 @@ const createSessionConfig = require('./config/session');
 const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
+const checkAuthStatusMiddleware = require('./middlewares/check-auth');
 const authRoutes = require('./routes/auth.routes');
-const productRoutes = require('./routes/products.routes');
+const productsRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
 
 const app = express();
@@ -24,13 +24,14 @@ app.use(express.urlencoded({ extended: false }));
 const sessionConfig = createSessionConfig();
 
 app.use(expressSession(sessionConfig));
-
 app.use(csrf());
+
 app.use(addCsrfTokenMiddleware);
+app.use(checkAuthStatusMiddleware);
 
 app.use(baseRoutes);
 app.use(authRoutes);
-app.use(productRoutes);
+app.use(productsRoutes);
 
 app.use(errorHandlerMiddleware);
 
@@ -39,6 +40,6 @@ db.connectToDatabase()
     app.listen(3000);
   })
   .catch(function (error) {
-    console.log('Failed to connect to the database');
+    console.log('Failed to connect to the database!');
     console.log(error);
   });
